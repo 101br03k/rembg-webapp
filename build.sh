d09c5c9068803e3a9487d897e7c2bd84350edf60
101br03k <warmerdamm03@gmail.com>
2024-10-31 21:08:01 +0100

    updated base image version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker compose down
-docker image rm ghcr.io/101br03k/rembg:v1.0-pv-3.12.5-slim
-docker build . -t ghcr.io/101br03k/rembg:v1.0-pv-3.12.5-slim
+docker image rm ghcr.io/101br03k/rembg:v1.1-pv-3.12.7-slim
+docker build . -t ghcr.io/101br03k/rembg:v1.1-pv-3.12.7-slim
 docker compose up -d
