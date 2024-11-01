458d0330fd426bb7a2ccccc61431430e4b099ff1
101br03k <warmerdamm03@gmail.com>
2024-11-01 21:02:07 +0100

    fixed typo

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def upload_file():
             img_io = BytesIO()
             output_image.save(img_io, 'PNG')
             img_io.seek(0)
-            # return send_file(img_io, mimetype='image/png')  # Change download in separatre browser tab
+            # return send_file(img_io, mimetype='image/png')  # Change download in separate browser tab
             return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='_rmbg.png')
     return render_template('index.html')
 
